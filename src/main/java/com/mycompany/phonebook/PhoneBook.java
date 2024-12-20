8133a7187b0e2e2f44bacf55c7ccf095cf3e0ae4
ricvigi <ricvigi@proton.me>
2024-12-20 14:52:47 +0100

    initial commit

---
+++ src/main/java/com/mycompany/phonebook/PhoneBook.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.phonebook;
+
+/**
+ *
+ * @author rick
+ */
+public class PhoneBook {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
