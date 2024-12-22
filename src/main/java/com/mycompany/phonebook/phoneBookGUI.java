46bab06e999dba76938bd0a47c4992eec9b6a909
ricvigi <ricvigi@proton.me>
2024-12-23 00:47:08 +0100

    Removed some other comments

--- src/main/java/com/mycompany/phonebook/phoneBookGUI.java
+++ src/main/java/com/mycompany/phonebook/phoneBookGUI.java
@@ -243,11 +243,7 @@ public class phoneBookGUI extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_jButton3ActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
